e28f4a86a900c9abe43d008c5b13155b3f5e0780
fabiothiroki <fabiothiroki@gmail.com>
2012-02-14 17:45:45 -0200

    skypelist

---
+++ skypelist/models.py
@@ -0,0 +1,7 @@
+# -*- coding:utf8 -*-
+
+from django.db import models
+
+class skypeuser(models.Model):
+	ramal = models.PositiveIntegerField(unique=True,blank=False)
+	username = models.CharField(max_length=50,blank=False,verbose_name='Endereço',unique=True)
